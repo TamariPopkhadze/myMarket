829ca5688241ef546bb4bb305a19a488d3178cf6
TamariPopkhadze <popkhadzetamari@gmail.com>
2023-07-30 14:56:08 +0400

    add auth method

---
+++ src/middlewares/checkAuthMidlleware.js
@@ -0,0 +1,21 @@
+import jwt from "jsonwebtoken";
+import { UnauthorizedException } from "../tool/errors";
+
+const CheckAuthMiddleware= (req, res, next) => {
+    try {
+      if (req.headers.hasOwnProperty('authorization')) {
+        const token = req?.headers?.authorization.split(' ')[1];
+  
+        jwt.verify(token, process.env.JWT_SECRET);
+  
+        return next();
+      } else {
+        throw new Error()
+      }
+    } catch (e) {
+      return next(new UnauthorizedException('please sign in'))
+    }
+  };
+  export default CheckAuthMiddleware;
+
+  
--- src/router/router.js
+++ src/router/router.js
@@ -1,10 +1,13 @@
 import express from 'express';
 import authRouter from './auth.router';
+import CheckAuthMiddleware from '../middlewares/checkAuthMidlleware';
 
 const router = express.Router();
 
 router.use("/auth",authRouter)
 
+router.use(CheckAuthMiddleware)
+router.use("/product",)
 
 
 
